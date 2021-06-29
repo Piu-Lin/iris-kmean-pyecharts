4b3638c0eec841294de894825daa38d4a6a04c8f
PiuLinx <15990799120@163.com>
2021-06-29 16:11:26 +0800

    1.0版本

--- Analysis.py
+++ Analysis.py
@@ -9,17 +9,42 @@ from sklearn.cluster import KMeans
 from sklearn import preprocessing
 
 
-def getCalyxLAW(data):  # getCalylenghAndWidth 获取花萼的长宽
+def getCalyLAW(data):  # getCalyLenghAndWidth 获取花萼的长宽
     return data[["id", "Sepal.Length", "Sepal.Width"]].values
 
 
-def scatterImgCreater(scatterdata):
-    sepalScatter = Scatter()  # 初始化散点图类
+def getPetalLAW(data):  # getpetalLenghAndWidth 获取花萼的长宽
+    return data[["id", "Petal.Length", "Petal.Width"]].values
+
+
+def scatterImgCreater(scatterdata, title):
+    sepalScatter = Scatter(init_opts=opts.InitOpts(
+        width="1600px", height="1000px"))  # 初始化散点图类
+    clusterTotal = len(scatterdata)  # 聚类的总个数
+    clusterNum = 0
     for eachcluster in scatterdata:
+        eachcluster = sorted(eachcluster, key=lambda x: (x[1], x[2]))
+        clusterNum += 1
         transponcluster = list(zip(*eachcluster))  # 转置原数据集以提取长度与宽度
         iddata = list(transponcluster[0])  # 提取id数据 或许不使用
         lenthdata = list(transponcluster[1])  # 提取长度数据
         widthdata = list(transponcluster[2])  # 提取宽度数据
+        sepalScatter.add_xaxis(
+            xaxis_data=lenthdata,
+        )
+        sepalScatter.add_yaxis(
+            series_name="",
+            y_axis=widthdata,
+            symbol_size=20,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        sepalScatter.set_global_opts(
+            yaxis_opts=opts.AxisOpts(
+                min_='dataMin'
+            ),
+            toolbox_opts=opts.ToolboxOpts(),
+        )
+    sepalScatter.render(title+"分类图.html")
 
 
 def analSepal(sepdata):  # 基于花萼进行分析
@@ -30,8 +55,23 @@ def analSepal(sepdata):  # 基于花萼进行分析
     kModel.fit(kmeansTraindata)  # 进行训练
     cluster = kModel.predict(kmeansTraindata)  # 生成聚类
     sepdata["cluster"] = cluster  # 添加到原数据中
-    eachClass = sepdata.groupby("cluster").apply(getCalyxLAW)  # 对数据进行分类
-    scatterImgCreater(eachClass)
+    eachClass = sepdata.groupby("cluster").apply(getCalyLAW)  # 对数据进行分类
+    title = "基于花萼进行分析"  # 图片标题
+    scatterImgCreater(eachClass, title)
+
+
+def analPetal(petdata):  # 基于花瓣进行分析
+    kmeansTraindata = pd.DataFrame(petdata)  # 拷贝一份作为训练集
+    kModel = KMeans(n_clusters=3)  # 实例化对象
+    min_max_scaler = preprocessing.MinMaxScaler()  # 设置定标器
+    kmeansTraindata = min_max_scaler.fit_transform(kmeansTraindata)  # 数据归一化
+    kModel.fit(kmeansTraindata)  # 进行训练
+    cluster = kModel.predict(kmeansTraindata)  # 生成聚类
+    petdata["cluster"] = cluster  # 添加到原数据中
+    eachClass = petdata.groupby("cluster").apply(getPetalLAW)  # 对数据进行分类
+    title = "基于花瓣进行分析"  # 图片标题
+    scatterImgCreater(eachClass, title)
+    # print(petdata)
 
 
 if __name__ == "__main__":
@@ -40,3 +80,4 @@ if __name__ == "__main__":
     print(a.columns)
     # 检查显示行数据
     analSepal(a[["id", "Sepal.Length", "Sepal.Width"]])
+    analPetal(a[["id", "Petal.Length", "Petal.Width"]])
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # iris-kmean-pyecharts
-鸢尾花分类基于k-mean与pyecharts实现
+
+鸢尾花分类基于 k-mean 与 pyecharts 实现
+
+pyeachrs 操作出现未知 bug 需使用数据视图刷新后实现。
